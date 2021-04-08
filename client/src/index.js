fe4f8ceb7e012749aab8891cf1799fc06c85afd1
Eric Benedetto <ericsplace3@gmail.com>
2021-04-08 13:17:57 -0400

    add client routes

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'fontsource-roboto';
 import './style.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Header } from './components/Header';
+import { HomeContainer } from './containers/HomeContainer';
+import { CreateContainer } from './containers/CreateContainer';
+import { TrackingContainer } from './containers/TrackingContainer';
+import { LoginContainer } from './containers/LoginContainer';
 
-ReactDOM.render(<p>Snatcher</p>, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <Header />
+        <Switch>
+            <Route path='/' component={HomeContainer} exact />
+            <Route path='/create/' component={CreateContainer} exact />
+            <Route path='/track/' component={TrackingContainer} exact />
+            <Route path='/track/:slug' component={TrackingContainer} exact />
+            <Route path='/login/' component={LoginContainer} exact />
+        </Switch>
+    </Router>,
+    document.getElementById('root')
+);
