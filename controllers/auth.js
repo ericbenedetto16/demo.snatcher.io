f4c5e824df981fbfb0b676feb7dadd46b4b1427d
Jonathan Shitrit <Jonathanshitrit8@gmail.com>
2021-04-25 16:01:36 -0400

    working on auth

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,27 +1,35 @@
-const { User } = require("../models")
+const router = require('express').Router();
+const { User } = require('../models');
+const passport = require('../middlewares/authentication');
 
-exports.createUser = async (req, res) => {
-    try {
-        const { user } = req.body;
-        console.log(req.body);
 
-        // // TODO: Check for Collisions
-        const obj = await User.create({
-            firstName: user.firstName,
-            lastName: user.lastName,
+router.post('/signup', (req, res) => {
+    console.log("POST body: ", req.body);
+    User.create({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password,
+    })
+        .then((user) => {
+            req.login(user, () => res.status(201).json(user));
+        })
+        .catch((err) => {
+            res.status(400).json({ msg: 'Failed Signup', err });
         });
+});
 
-        res.status(200).json({
-            success: true,
-            firstName: obj.firstName,
-            lastName: obj.lastName
-        });
-    } catch (err) {
-        // eslint-disable-next-line no-console
-        console.log(`${err}`.red);
-        res.status(500).json({
-            success: false,
-            msg: 'Internal Server Error!',
-        });
-    }
-};
+router.post('/login',
+    passport.authenticate('local'),
+    (req, res) => {
+        // If this function gets called, authentication was successful.
+        // `req.user` contains the authenticated user.
+        res.json(req.user);
+    });
+
+router.post('/logout', (req, res) => {
+    req.logout();
+    res.status(200).json({ message: 'Logout successful' });
+})
+
+module.exports = router;
