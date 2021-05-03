b5ea7e78bedd8edec090fcbb7b7cf2f389c053a3
Eric Benedetto <ericsplace3@gmail.com>
2021-05-03 16:04:46 -0400

    proxy scripts for do-cert-manager issues

---
+++ kubernetes/scripts/disableProxy.sh
@@ -0,0 +1,7 @@
+kubectl patch svc ingress-nginx-controller -n ingress-nginx -p "{\"metadata\":{\"labels\":{\"service.beta.kubernetes.io/do-loadbalancer-enable-proxy-protocol\":\"false\"}}}"
+
+kubectl patch svc ingress-nginx-controller -n ingress-nginx -p "{\"metadata\":{\"annotations\":{\"service.beta.kubernetes.io/do-loadbalancer-enable-proxy-protocol\":\"false\"}}}"
+
+kubectl edit configmap ingress-nginx-controller -n ingress-nginx # Manually Update Values for Proxy in Config
+
+# kubectl logs -n cert-manager cert-manager-7dd5854bb4-vfzxm
