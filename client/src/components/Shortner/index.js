f8dd89c478b59866d453668250f087f103eac327
Eric Benedetto <ericsplace3@gmail.com>
2021-05-03 16:01:48 -0400

    add domain dropdown to shortner

--- client/src/components/Shortner/index.js
+++ client/src/components/Shortner/index.js
@@ -5,6 +5,7 @@ import {
     InputAdornment,
     TextField,
 } from '@material-ui/core';
+import { Autocomplete } from '@material-ui/lab';
 import FileCopyIcon from '@material-ui/icons/FileCopy';
 import { createLink } from '../../api/queries';
 
@@ -21,9 +22,37 @@ const urlValidator = (url) => {
     }
 };
 
+// eslint-disable-next-line operator-linebreak
+const DEFAULT_DOMAIN =
+    process.env.NODE_ENV === 'development'
+        ? 'http://localhost:8080'
+        : 'https://shrtnd.xyz';
+
+const DOMAINS = [
+    {
+        name: 'https://cmprsd.link',
+    },
+    {
+        name: 'https://minified.live',
+    },
+    {
+        name: 'https://shrinkd.xyz',
+    },
+    {
+        name: 'https://shrtnd.xyz',
+    },
+    {
+        name: 'https://shrtnr.io',
+    },
+    {
+        name: 'https://tinyd.live',
+    },
+];
+
 export const Shortner = () => {
     const [url, setUrl] = useState('');
     const [shortened, setShortened] = useState(null);
+    const [domain, setDomain] = useState(DEFAULT_DOMAIN);
 
     return (
         <>
@@ -48,6 +77,7 @@ export const Shortner = () => {
                         variant='outlined'
                         helperText='Enter a Valid URL'
                         onChange={(e) => setUrl(e.target.value)}
+                        style={{ width: '250px' }}
                     />
                 </div>
                 <div>
@@ -62,30 +92,62 @@ export const Shortner = () => {
                 </div>
             </form>
             {shortened ? (
-                <TextField
-                    id='generated-url'
-                    label='Shortened URL'
-                    readOnly
-                    value={`http://localhost:8080/${shortened}`}
-                    style={{ marginLeft: 25, marginTop: 10, width: '300px' }}
-                    InputProps={{
-                        endAdornment: (
-                            <InputAdornment position='end'>
-                                <IconButton
-                                    aria-label='Copy to Clipboard'
-                                    onClick={() => {
-                                        document
-                                            .querySelector('#generated-url')
-                                            .select();
-                                        document.execCommand('copy');
-                                    }}
-                                >
-                                    <FileCopyIcon />
-                                </IconButton>
-                            </InputAdornment>
-                        ),
-                    }}
-                />
+                <>
+                    <Autocomplete
+                        id='domain-autocomplete'
+                        options={DOMAINS}
+                        getOptionLabel={(opt) => opt.name}
+                        getOptionSelected={(opt) => opt.name}
+                        onChange={(e, v) => {
+                            if (v === '' || v === null) {
+                                setDomain(DEFAULT_DOMAIN);
+                            } else {
+                                setDomain(v.name);
+                            }
+                        }}
+                        value={{ name: domain || DEFAULT_DOMAIN }}
+                        style={{
+                            width: '250px',
+                            marginTop: '10px',
+                            marginLeft: '25px',
+                        }}
+                        renderInput={(params) => (
+                            <TextField
+                                {...params}
+                                label='Domain'
+                                variant='outlined'
+                            />
+                        )}
+                    />
+                    <TextField
+                        id='generated-url'
+                        label='Shortened URL'
+                        readOnly
+                        value={`${domain}/${shortened}`}
+                        style={{
+                            marginLeft: 25,
+                            marginTop: 10,
+                            width: '300px',
+                        }}
+                        InputProps={{
+                            endAdornment: (
+                                <InputAdornment position='end'>
+                                    <IconButton
+                                        aria-label='Copy to Clipboard'
+                                        onClick={() => {
+                                            document
+                                                .querySelector('#generated-url')
+                                                .select();
+                                            document.execCommand('copy');
+                                        }}
+                                    >
+                                        <FileCopyIcon />
+                                    </IconButton>
+                                </InputAdornment>
+                            ),
+                        }}
+                    />
+                </>
             ) : (
                 []
             )}
